47d2ab994c2ffd7f13140c3e43cb8080fc95132c
Lav Dwivedi <lavdwivedi09@gmail.com>
2026-06-03 11:51:51 +0530

    Create Question_2.cpp

---
+++ Week_1/Day_3/Question_2.cpp
@@ -0,0 +1,19 @@
+// 3300. Minimum Element After Replacement With Digit Sum 
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+        int n = nums.size() ;
+        int ans = INT_MAX ;
+        for(int i=0;i<n;i++){
+           int x = nums[i];
+           int sum = 0 ;
+           while(x>0){
+            sum += x%10 ;
+            x /= 10 ;
+           } 
+           ans = min(ans,sum);
+
+        }
+        return ans ;
+    }
+};
