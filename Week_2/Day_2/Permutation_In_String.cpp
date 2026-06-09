e2b24dd36f9bc713832d116bd8d42ba8f2056941
Lav Dwivedi <lavdwivedi09@gmail.com>
2026-06-09 18:21:35 +0530

    Create Permutation_In_String.cpp

---
+++ Week_2/Day_2/Permutation_In_String.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isFreqSame(int freq1[],int freq2[] ){
+        for(int i=0;i<26;i++){
+            if(freq1[i]!=freq2[i]){
+                return false ;
+            }
+        }
+        return true  ;
+    }
+
+    bool checkInclusion(string s1, string s2) {
+        int freq[26] = {0};
+        for (int i = 0; i < s1.length(); i++) {
+            freq[s1[i] - 'a']++;  // convert into. ASCII value 
+        }
+        int windsize = s1.length();
+        for (int i = 0; i < s2.length(); i++) {
+            int windIdx = 0, Idx = i;
+            int windfreq[26] = {0};
+            while (windIdx < windsize && Idx < s2.length()) {
+                windfreq[s2[Idx] - 'a']++;
+                windIdx++;
+                Idx++;
+            }
+            if(isFreqSame(freq , windfreq)){
+                return true  ;
+            }
+        }
+        return false ;
+    }
+};
