a32d78659ad19ffd8dcf333c1364fc020bb3b4aa
Lav Dwivedi <lavdwivedi09@gmail.com>
2026-06-11 08:01:40 +0530

    Create Is_Subsequence.cpp

---
+++ Week_2/Day_3/Is_Subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n1 = s.size();
+        int n2 = t.size();
+        int j = 0 ;
+        for(int i=0;i<n2;i++){
+            if(t[i]==s[j]){
+                j++ ;
+            }
+            if(j==n1) return true  ;
+        }
+        return (j==n1) ;
+    }
+}; 
